cfd35aae1f11c2cae5c2ad0a252b789fbc5b345a
Deanang <ngeekeng@gmail.com>
2020-05-27 22:07:44 +0800

    include build
    include build

---
+++ build/precache-manifest.fabeacb07ad21a88c052d172a2f2e2ae.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c748d8b0c2e6a04f7a56c422d81a0150",
+    "url": "/index.html"
+  },
+  {
+    "revision": "048e7bb6431d6912f284",
+    "url": "/static/js/2.7735b5bc.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.7735b5bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae406ffec0d6fbd67302",
+    "url": "/static/js/main.86d325c5.chunk.js"
+  },
+  {
+    "revision": "5327e7535f30670ee55d",
+    "url": "/static/js/runtime-main.88eed8d2.js"
+  }
+]);
